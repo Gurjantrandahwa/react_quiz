7d9a95d3cebadff6760e1fb69306ace025fb4267
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-01-16 11:19:30 +0530

    quiz game implemented

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -1,20 +1,35 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./quiz.scss";
-import {questions} from "../questions";
+import {questions} from "./questions";
+
 export default function Quiz() {
-const [question,setQuestion]=useState(questions)
+    const [answers, setAnswers] = useState(0)
+    const [currentQuestion, setCurrentQuestion] = useState(0);
     return <div className={"quiz-wrapper"}>
 
         <h1>Quiz Game</h1>
+        <img src={"https://justacoding.blog/wp-content/uploads/2021/11/quiz.jpg"}/>
         <div className={"quiz-card"}>
-            {question.map((value,index)=>{
-                return<div key={index} className={"questions"}>
-                   <h2> Q: {value.question}</h2>
-                   <button>{value.options[0]}</button>
-                   <button>{value.options[1]}</button>
-                   <button>{value.options[2]}</button>
-                </div>
-            })}
+
+
+            {
+                questions.map((value, index) => {
+                    return <div key={index}
+                                className={"questions"}>
+                        <h2> Q: {value.question}</h2>
+                        <div>
+                            <button>{value.options[0]}</button>
+                            <button>{value.options[1]}</button>
+                            <button>{value.options[2]}</button>
+                        </div>
+
+                    </div>
+                })
+
+            }
+          on
+
         </div>
+
     </div>
 }
---
+++ src/Quiz/questions.js
@@ -0,0 +1,25 @@
+export const questions = [
+    {
+        question: "In what contintent is Indonesia?",
+        answers: ["South America", "Europe", "Asia"],
+        correctAnswerIndex: 2
+    },
+    {
+
+        question: `Who is the CEO of Tesla Motors?`,
+        answers: [`Bill Gates`, `Steve Jobs`, `Elon Musk`],
+        correctAnswerIndex: 3
+    },
+    {
+
+        question: `Name World's Richest Man?`,
+        answers: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
+        correctAnswerIndex:1
+    },
+    {
+
+        question: `World's Longest River?`,
+        answers: [`River Nile`, `River Amazon`, `River Godavari`],
+        correctAnswerIndex: 1
+    }
+];
